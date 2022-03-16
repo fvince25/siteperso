d2c3dfad58c00c1be449a4fb6c3567acfe2a1694
Vincent Fritz <fvince25@yahoo.fr>
2022-03-16 08:46:46 +0100

    Annulation du knp menu, (maintien de la classe), création de l'entité NavbarMenu, crée mig, migration.

---
+++ project/src/Entity/NavbarMenu.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NavbarMenuRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NavbarMenuRepository::class)
+ */
+class NavbarMenu
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $item;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $label;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdat;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getItem(): ?string
+    {
+        return $this->item;
+    }
+
+    public function setItem(string $item): self
+    {
+        $this->item = $item;
+
+        return $this;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getCreatedat(): ?\DateTimeInterface
+    {
+        return $this->createdat;
+    }
+
+    public function setCreatedat(\DateTimeInterface $createdat): self
+    {
+        $this->createdat = $createdat;
+
+        return $this;
+    }
+}
